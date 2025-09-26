9672fd55605f5d7a39e4b8104e9f4a05d5d0b112
jonawoning <jonawoningyt@gmail.com>
2025-09-26 16:15:26 +0200

    Toevoegen email validatie
    - Toevoegen van email validatie op basis van System.Net.Mail.Mailadress

---
+++ Grocery.Core/Helpers/EmailHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Grocery.Core.Helpers;
+
+public class EmailHelper
+{
+    
+    public static bool ValidateEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+}

2a38e78f80d4ed4b332b75a39cc1bfbff5aafaae
jonawoning <jonawoningyt@gmail.com>
2025-09-26 15:37:53 +0200

    Boodschappen lijst titel veranderd naar Client Name
    - Client.Name is nu de titel van i.p.v. de boodschappenlijst.

--- Grocery.App/ViewModels/GroceryListViewModel.cs
+++ Grocery.App/ViewModels/GroceryListViewModel.cs
@@ -10,12 +10,14 @@ namespace Grocery.App.ViewModels
     {
         public ObservableCollection<GroceryList> GroceryLists { get; set; }
         private readonly IGroceryListService _groceryListService;
+        private readonly GlobalViewModel _global;
 
-        public GroceryListViewModel(IGroceryListService groceryListService) 
+        public GroceryListViewModel(IGroceryListService groceryListService, GlobalViewModel global) 
         {
-            Title = "Boodschappenlijst";
             _groceryListService = groceryListService;
+            _global = global;
             GroceryLists = new(_groceryListService.GetAll());
+            Title = $"Boodschappen lijst van {_global.Client.Name}";
         }
 
         [RelayCommand]
